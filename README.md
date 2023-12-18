e7f95658e273d21c6253608fc495fc722d5b8cb2
Mayara Vasques <mrv.ele23@uea.edu.br>
2023-12-18 13:03:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# may
+ Portifilio empresarial
---
+++ exemplos.js
@@ -0,0 +1,81 @@
+{
+    "projects": {}
+      {
+        "id": 1,
+        "name": "EcoTech Energy",
+        "iconUrl": "https://example.com/icon-eco-tech.png",
+        "description": "EcoTech Energy is a pioneering initiative focused on integrating energy efficiency solutions in industrial processes. It involves the development of smart energy monitoring and control systems, aiming at sustainability and innovation.",
+        "startDate": "2023-01-15",
+        "endDate": "2023-12-20",
+        "projectLink": "https://ecotechenergia.com",
+        "tags": ["Energy Efficiency", "Industry 4.0", "Sustainability"],
+        "companyName": "EcoTech Solutions",
+        "companyIconUrl": "https://example.com/icon-eco-tech-company.png",
+        "members": [
+          {
+            "name": "John Silva",
+            "role": "Chief Engineer",
+            "description": "Responsible for developing innovative energy efficiency solutions.",
+            "photoUrl": "https://example.com/photo-john.png"
+          },
+          {
+            "name": "Mary Fernandes",
+            "role": "Project Manager",
+            "description": "Coordinates all stages of the project, ensuring timely delivery.",
+            "photoUrl": "https://example.com/photo-mary.png"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Advanced Industry 4.0",
+        "iconUrl": "https://example.com/icon-industry-4-0.png",
+        "description": "Advanced Industry 4.0 focuses on elevating industrial automation to new heights. It involves implementing cyber-physical systems, IoT, and big data analysis to transform the industrial production chain, enhancing efficiency and digitalization.",
+        "startDate": "2023-03-22",
+        "endDate": "2024-04-10",
+        "projectLink": "https://industria4-0.com",
+        "tags": ["Industry 4.0", "Process Improvement", "Technology"],
+        "companyName": "Advanced Industrial Tech",
+        "companyIconUrl": "https://example.com/icon-adv-ind-tech.png",
+        "members": [
+          {
+            "name": "Carlos Gomes",
+            "role": "IoT Specialist",
+            "description": "Leads the development and integration of IoT solutions in the industrial environment.",
+            "photoUrl": "https://example.com/photo-carlos.png"
+          },
+          {
+            "name": "Ana Beatriz",
+            "role": "Data Analyst",
+            "description": "Analyzes large volumes of data to optimize production processes.",
+            "photoUrl": "https://example.com/photo-ana.png"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Green Future",
+        "iconUrl": "https://example.com/icon-green-future.png",
+        "description": "Green Future is an ambitious project focused on developing and implementing sustainable technologies for a greener, cleaner future. It covers renewable energy generation to sustainable mobility solutions, aiming to create a positive environmental impact through green technological innovations.",
+        "startDate": "2023-05-10",
+        "endDate": "2023-11-30",
+        "projectLink": "https://greenfuture.com",
+        "tags": ["Sustainability", "Eco-Friendly", "Green Technology"],
+        "companyName": "Green Innovations",
+        "companyIconUrl": "https://example.com/icon-green-innovations.png",
+        "members": [
+          {
+            "name": " Specialist",
+            "description": "Focuses on developRicardo Souza",
+            "role": "Renewable Energying new technologies for clean energy generation.",
+            "photoUrl": "https://example.com/photo-ricardo.png"
+          },
+          {
+            "name": "Livia Martins",
+            "role": "Environmental Engineer",
+            "description": "Works on integrating sustainable practices throughout the project.",
+            "photoUrl": "https://example.com/photo-livia.png"
+          }
+        ]
+      },
+      {
